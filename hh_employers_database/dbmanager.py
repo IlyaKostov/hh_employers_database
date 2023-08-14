3120e7f600892ece89797f714a686261bf185061
Kostov Ilya <kick1991@mail.ru>
2023-08-14 18:23:57 +0500

    refactoring classes, relocate DBManager in dbmanager.py, add queries.sql

--- hh_employers_database/dbmanager.py
+++ hh_employers_database/dbmanager.py
@@ -1,48 +1,7 @@
 import os
-import time
 from configparser import ConfigParser
 
 import psycopg2
-import requests
-
-
-class HeadHunterAPI:
-
-    EMPLOYER_ID = ['1740', '15478', '2180', '64174', '84585', '1057', '3776', '3529', '78638', '41862']
-    URL_VACANCIES = "https://api.hh.ru/vacancies"
-    URL_EMPLOYERS = "https://api.hh.ru/employers/{employer_id}"
-    MAX_PAGES = 2
-
-    def get_employers(self):
-        employers = []
-        for employer in self.EMPLOYER_ID:
-
-            employer_data = requests.get(self.URL_EMPLOYERS.format(employer_id=employer)).json()
-            employers.append(employer_data)
-
-            time.sleep(0.25)
-        return employers
-
-    def get_vacancies(self):
-        """
-        Получение списка вакансий с платформы HeadHunter
-        :return: list
-        """
-        vacancies_lst = []
-        for employer in self.EMPLOYER_ID:
-            for page in range(self.MAX_PAGES):
-                params = {'per_page': 2,
-                          'page': page,
-                          'order_by': "publication_time",
-                          'area': [113, 16, 40],
-                          'employer_id': employer
-                          }
-                vacancies = requests.get(self.URL_VACANCIES, params=params).json()
-                vacancies_lst.extend(vacancies['items'])
-                if (vacancies['pages'] - page) <= 1:
-                    break
-                time.sleep(0.2)
-        return vacancies_lst
 
 
 class DBManager:
@@ -58,10 +17,8 @@ class DBManager:
         params = parser.items(section)
         for param in params:
             db[param[0]] = param[1]
-        try:
-            self.create_database(db, db_name)
-        except psycopg2.ProgrammingError:
-            print(f'Подключаюсь к базе данных {db_name}')
+
+        self.create_database(db, db_name)
 
         db.update({'dbname': db_name})
 
@@ -69,14 +26,12 @@ class DBManager:
         self.connection = conn
         self.cursor = conn.cursor()
 
-    def query(self, string: str, params: tuple):
+    def query(self, string: str, params: tuple) -> None:
         """Выполняет запросы к sql на запись"""
         if params:
             self.cursor.execute(string, params)
         else:
             self.cursor.execute(string)
-
-        self.connection.commit()
 
     def get_companies_and_vacancies_count(self) -> list:
         """Получает список всех компаний и количество вакансий у каждой компании."""
@@ -143,15 +98,8 @@ class DBManager:
         conn.set_session(autocommit=True)
         cur = conn.cursor()
 
+        cur.execute(f"DROP DATABASE  IF EXISTS {db_name};")
         cur.execute(f"CREATE DATABASE {db_name};")
 
         cur.close()
         conn.close()
-
-
-# data_base_name = 'my_new_db'
-# psql = DBManager(data_base_name)
-#
-# psql.close_cursor()
-# psql.close_connection()
-# print()
